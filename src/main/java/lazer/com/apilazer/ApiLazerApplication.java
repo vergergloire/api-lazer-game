c6d50e630a0dddc8c63e718c14867f04fe6b67d0
vergergloire <demain.moussavou@gmail.com>
2025-02-28 15:30:06 +0100

    commit initial

---
+++ src/main/java/lazer/com/apilazer/ApiLazerApplication.java
@@ -0,0 +1,13 @@
+package lazer.com.apilazer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLazerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiLazerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lazer/com/apilazer/ApiLazerApplicationTests.java
@@ -0,0 +1,13 @@
+package lazer.com.apilazer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLazerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
